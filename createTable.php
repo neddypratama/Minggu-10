65abaa1116b80b068f90d94ed6bdcee084b28e9b
neddypratama <neddy_pratama@yahoo.com>
2022-04-25 00:47:56 +0700

    Minggu 10

---
+++ createTable.php
@@ -0,0 +1,27 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "web_db";
+
+    $connect = mysqli_connect($hostname, $username, $password, $database);
+
+    if($connect) {
+        echo "Koneksi ke MySQL berhasil! <br>";
+    } else {
+        echo "Koneksi ke MySQL gagal!";
+    }
+
+    $query = "CREATE TABLE strudent (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(30) NOT NULL,
+        address VARCHAR(100))";
+
+    if(mysqli_query($connect, $query)) {
+        echo "Tabel  student berhasil dibuat!";
+    } else {
+        echo "Tabel student gagal dibuat <br>". mysqli_error($connect);
+    }
+
+    mysqli_close($connect);
+?>
